134e68bc4c1d06ecb009f3a85f84d035ba8a2688
speleo <henry@hbennett.com>
2015-07-19 19:45:27 +0100

    Initial upload from Henry B

---
+++ bluewater-v7/thTMPDIR/data.pl
@@ -0,0 +1,125 @@
+%SCRAP = (
+	# scrap: bluewater-above-climb-2
+	aaaaaaa => {
+		 F => "data.1",
+		 B => "data.2",
+		 I => "data.2bg",
+		 C => "data.2clip",
+		 E => "data.3",
+		 X => "data.4",
+		 P => "data.4bbox",
+		 Y => 1,
+		 V => -4,
+		 S => "3752.41 -473.67",
+	},
+	# scrap: bluewater-above-climb-1-S2
+	aaaaaab => {
+		 F => "data.5",
+		 B => "data.6",
+		 I => "data.6bg",
+		 C => "data.6clip",
+		 E => "data.7",
+		 X => "data.8",
+		 P => "data.8bbox",
+		 Y => 1,
+		 V => -4,
+		 S => "3475.09 -524.68",
+	},
+	# scrap: bluewater-above-climb-1-S1
+	aaaaaac => {
+		 F => "data.9",
+		 B => "data.10",
+		 I => "data.10bg",
+		 C => "data.10clip",
+		 E => "data.11",
+		 X => "data.12",
+		 P => "data.12bbox",
+		 Y => 1,
+		 V => -4,
+		 S => "1875.54 -783.16",
+	},
+	# scrap: bluewater-above-olympic-pool
+	aaaaaad => {
+		 F => "data.13",
+		 B => "data.14",
+		 I => "data.14bg",
+		 C => "data.14clip",
+		 E => "data.15",
+		 X => "data.16",
+		 P => "data.16bbox",
+		 Y => 1,
+		 V => -4,
+		 S => "1055.75 -1145.86",
+	},
+	# scrap: bluewater-fossil
+	aaaaaae => {
+		 F => "data.17",
+		 B => "data.18",
+		 I => "data.18bg",
+		 C => "data.18clip",
+		 E => "data.19",
+		 X => "data.20",
+		 P => "data.20bbox",
+		 Y => 1,
+		 V => -3,
+		 S => "1121.85 -1225.96",
+	},
+	# scrap: bluewater-olympic-pool
+	aaaaaaf => {
+		 F => "data.21",
+		 B => "data.22",
+		 I => "data.22bg",
+		 C => "data.22clip",
+		 E => "data.23",
+		 Y => 1,
+		 V => -3,
+		 S => "637.50 -1679.25",
+	},
+	# scrap: bluewater-entrance-olympic-pool
+	aaaaaag => {
+		 F => "data.24",
+		 B => "data.25",
+		 I => "data.25bg",
+		 C => "data.25clip",
+		 E => "data.26",
+		 X => "data.27",
+		 P => "data.27bbox",
+		 Y => 1,
+		 V => -3,
+		 S => "477.64 -1721.74",
+	},
+	# scrap: bluewater-entrance_detail
+	aaaaaah => {
+		 F => "data.28",
+		 B => "data.29",
+		 I => "data.29bg",
+		 C => "data.29clip",
+		 E => "data.30",
+		 Y => 1,
+		 V => -2,
+		 S => "90.01 -1881.58",
+	},
+	# scrap: bluewater-entrance-overlay-passage
+	aaaaaai => {
+		 F => "data.31",
+		 B => "data.32",
+		 I => "data.32bg",
+		 C => "data.32clip",
+		 E => "data.33",
+		 Y => 1,
+		 V => -1,
+		 S => "86.85 -1912.83",
+	},
+);
+
+
+%LAYER = (
+	# expanded map: bluewaterM
+	1 => {
+		N => 'bluewaterM',
+	},
+);
+
+
+
+$PATTERN_DEFS = "./";
