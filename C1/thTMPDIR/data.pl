5377ed7201bf76710572228725eedd2c8cd2705c
speleo <henry@hbennett.com>
2015-11-16 09:18:57 +0000

    Updates to C1 mainly.  Correcting some sync errors

---
+++ C1/thTMPDIR/data.pl
@@ -0,0 +1,40 @@
+%SCRAP = (
+	# scrap: C1SE2
+	aaaaaaa => {
+		 B => "data.1",
+		 I => "data.1bg",
+		 C => "data.1clip",
+		 E => "data.2",
+		 X => "data.3",
+		 P => "data.3bbox",
+		 Y => 1,
+		 V => -1,
+		 S => "2104.20 -279.60",
+	},
+	# scrap: C1SE1
+	aaaaaab => {
+		 F => "data.4",
+		 B => "data.5",
+		 I => "data.5bg",
+		 C => "data.5clip",
+		 E => "data.6",
+		 X => "data.7",
+		 P => "data.7bbox",
+		 Y => 1,
+		 V => -1,
+		 S => "1409.97 -361.68",
+	},
+);
+
+
+%LAYER = (
+	# expanded map: C1ME
+	1 => {
+		N => 'C1ME',
+		T => 'C1ME',
+	},
+);
+
+
+
+$PATTERN_DEFS = "./";
